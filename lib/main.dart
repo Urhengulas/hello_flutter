292fd551077d4a4b75c563b86f9c2ea6654bda04
Urhengulas <joheihe@web.de>
2019-06-04 12:47:23 +0200

    Introduce hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    YourApp(),
+  );
+}
+
+class YourApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: "I am an app",
       home: Scaffold(
           backgroundColor: Colors.green[300],
@@ -14,6 +21,6 @@ void main() {
             child: Image.network(
                 'https://media04.meinanzeiger.de/article/2014/11/04/9/117079_L.jpg?1556343184'),
           )),
-    ),
-  );
+    );
+  }
 }
