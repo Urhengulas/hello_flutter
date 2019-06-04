eef8326b8e81eacec8b153478d6e0214bcec7761
Urhengulas <joheihe@web.de>
2019-06-04 14:59:12 +0200

    Build flutter layouts challenge app (from scratch)

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    YourApp(),
-  );
+  runApp(YourApp());
 }
 
 class YourApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "I am an app",
-      home: Scaffold(
-          backgroundColor: Colors.green[300],
-          appBar: AppBar(
-            title: Text("Anna Hemman zieht es in die Welt"),
-            backgroundColor: Colors.green[800],
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.teal,
+          body: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment
+                  .stretch, // preserves big containers from shrinking, if adding content
+              children: <Widget>[
+                Container(
+                  color: Colors.red,
+                  width: 100.0,
+                  child: Text("I am a red container."),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      color: Colors.yellow,
+                      width: 100.0,
+                      height: 100.0,
+                      child: Text("I am a yellow container."),
+                    ),
+                    Container(
+                      color: Colors.green,
+                      width: 100.0,
+                      height: 100.0,
+                      child: Text("I am a green container."),
+                    ),
+                  ],
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: 100.0,
+                  child: Text("I am a blue container."),
+                ),
+              ],
+            ),
           ),
-          body: Center(
-            child: Image.network(
-                'https://media04.meinanzeiger.de/article/2014/11/04/9/117079_L.jpg?1556343184'),
-          )),
+        ),
+      ),
     );
   }
 }
