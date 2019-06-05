46abb349749354598de63890095470327af20c1d
Urhengulas <joheihe@web.de>
2019-06-05 20:35:30 +0200

    Dicee rough structure

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,33 @@ class YourApp extends StatelessWidget {
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
-          body: Text("diceee"),
+          backgroundColor: Colors.red,
+          appBar: AppBar(
+            title: Center(child: Text("Dicee")),
+            backgroundColor: Colors.red,
+          ),
+          body: Center(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    child: Image.asset('img/dice1.png'),
+                    onPressed: (){
+                      print("Left button got pressed.");
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    child: Image.asset('img/dice3.png'),
+                    onPressed: (){
+                      print("Right button got pressed.");
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
