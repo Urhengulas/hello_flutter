65cd3449bef47a4ea31c2980b6f94dfe39904559
Urhengulas <joheihe@web.de>
2019-06-05 16:41:39 +0200

    Start building business card website

--- lib/main.dart
+++ lib/main.dart
@@ -11,41 +11,16 @@ class YourApp extends StatelessWidget {
       home: SafeArea(
         child: Scaffold(
           backgroundColor: Colors.teal,
-          body: Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment
-                  .stretch, // preserves big containers from shrinking, if adding content
-              children: <Widget>[
-                Container(
-                  color: Colors.red,
-                  width: 100.0,
-                  child: Text("I am a red container."),
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      color: Colors.yellow,
-                      width: 100.0,
-                      height: 100.0,
-                      child: Text("I am a yellow container."),
-                    ),
-                    Container(
-                      color: Colors.green,
-                      width: 100.0,
-                      height: 100.0,
-                      child: Text("I am a green container."),
-                    ),
-                  ],
-                ),
-                Container(
-                  color: Colors.blue,
-                  width: 100.0,
-                  child: Text("I am a blue container."),
-                ),
-              ],
-            ),
+          body: Column(
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage("img/johann.jpg"),
+              ),
+              Text(
+                "Johann Hemmann",
+                style: TextStyle(),
+              )
+            ],
           ),
         ),
       ),
