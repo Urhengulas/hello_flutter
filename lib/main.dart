474d3e238d707ce3a8e0597cf312c17f0776bb40
Urhengulas <joheihe@web.de>
2019-06-06 14:54:54 +0200

    Move safearea, scaffold, appbar to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(YourApp());
-}
-
-class YourApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       home: SafeArea(
         child: Scaffold(
           backgroundColor: Colors.red,
@@ -15,29 +10,36 @@ class YourApp extends StatelessWidget {
             title: Center(child: Text("Dicee")),
             backgroundColor: Colors.red,
           ),
-          body: Center(
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: FlatButton(
-                    child: Image.asset('img/dice1.png'),
-                    onPressed: (){
-                      print("Left button got pressed.");
-                    },
-                  ),
-                ),
-                Expanded(
-                  child: FlatButton(
-                    child: Image.asset('img/dice3.png'),
-                    onPressed: (){
-                      print("Right button got pressed.");
-                    },
-                  ),
-                )
-              ],
+          body: YourBody(),
+        ),
+      ),
+    ),
+  );
+}
+
+class YourBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('img/dice1.png'),
+              onPressed: () {
+                print("Left button got pressed.");
+              },
             ),
           ),
-        ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('img/dice3.png'),
+              onPressed: () {
+                print("Right button got pressed.");
+              },
+            ),
+          )
+        ],
       ),
     );
   }
