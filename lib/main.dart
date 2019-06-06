6dcc39fc6005f2d2a4aef026a1c779127e5ac62d
Urhengulas <joheihe@web.de>
2019-06-06 16:22:14 +0200

    Clear main.dart + get dependencies for magic8

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.red,
-          appBar: AppBar(
-            title: Center(child: Text("Dicee")),
-            backgroundColor: Colors.red,
-          ),
-          body: DicePage(),
-        ),
-      ),
-    ),
-  );
-}
-
-class DicePage extends StatefulWidget {
-  @override
-  _DicePageState createState() => _DicePageState();
-}
-
-class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 1;
-  int rightDiceNumber = 1;
-
-  void rollDice() {
-    setState(() {
-      leftDiceNumber = randDice();
-      rightDiceNumber = randDice();
-    });
-  }
-
-  int randDice() {
-    return Random().nextInt(6) + 1;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              child: Image.asset('img/dice$leftDiceNumber.png'),
-              onPressed: () {
-                rollDice();
-              },
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              child: Image.asset('img/dice$rightDiceNumber.png'),
-              onPressed: () {
-                rollDice();
-              },
-            ),
-          )
-        ],
+void main() => runApp(
+      MaterialApp(
+        home: Text("clear"),
       ),
     );
-  }
-}
